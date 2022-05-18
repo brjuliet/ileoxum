9fd588e938f2c5f3e931cd2bb3a68c22c52f76aa
brjuliet <juliet.inaraujo@gmail.com>
2022-05-18 10:15:58 -0300

    ajustes

--- src/app/ile-oxum/services/atendimentos.service.ts
+++ src/app/ile-oxum/services/atendimentos.service.ts
@@ -1,6 +1,6 @@
 import { environment } from './../../../environments/environment';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { take } from 'rxjs';
 
 @Injectable({
@@ -8,14 +8,30 @@ import { take } from 'rxjs';
 })
 export class AtendimentosService {
 
+
+
+
   constructor(private http: HttpClient) { }
 
   private readonly API = "http://localhost:3000/"
   private URL: string = environment.URL;
 
 
+
+
   getGuias(){
-    return this.http.get<any>(this.URL + `usuarios`);
+    let headers = new HttpHeaders({
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+    });
+
+    let httpOptions = {
+      headers: headers
+    };
+
+    return this.http.get<any>(this.URL + `usuarios`, httpOptions);
   }
 
 
