611aeae4e7e64ecbc5adc95204a1d46fc7733b28
brjuliet <juliet.inaraujo@gmail.com>
2022-05-18 01:00:21 -0300

    ajuste bd

--- src/app/ile-oxum/services/atendimentos.service.ts
+++ src/app/ile-oxum/services/atendimentos.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { take } from 'rxjs';
@@ -10,23 +11,24 @@ export class AtendimentosService {
   constructor(private http: HttpClient) { }
 
   private readonly API = "http://localhost:3000/"
+  private URL: string = environment.URL;
 
 
   getGuias(){
-    return this.http.get<any>(this.API + `usuarios`);
+    return this.http.get<any>(this.URL + `usuarios`);
   }
 
 
   postGira(body: any){
-    return this.http.post<any>(this.API + `giras`, body).pipe(take(1));
+    return this.http.post<any>(this.URL + `giras`, body).pipe(take(1));
   }
 
   getAtendimentos(){
-    return this.http.get<any>(this.API + `giras`);
+    return this.http.get<any>(this.URL + `giras`);
   }
 
   remove(id: number){
-    return this.http.delete(`${this.API}giras/${id}`).pipe(take(1));;
+    return this.http.delete(`${this.URL}giras/${id}`).pipe(take(1));;
   }
 
 }
